d6a72055982b69c481caa83f5291ca3b3a24b2f0
Crowdbotics <team@crowdbotics.com>
2023-01-25 16:42:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kurts-dance-party-38458",
+  url: "https://kurts-dance-party-38458.botics.co",
+  api: "https://kurts-dance-party-38458.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
